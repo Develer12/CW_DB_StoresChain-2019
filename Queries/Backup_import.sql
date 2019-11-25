f9f2ce4bbda3d5f90c34b9956d2feb6164ab669d
Develer12 <cenia-v@mail.ru>
2019-11-25 10:10:06 +0300

    Complete xml import

--- Queries/Backup_import.sql
+++ Queries/Backup_import.sql
@@ -40,3 +40,23 @@ SET @fileName = 'D:\Subject\BD_COURSE\backups\STORES_CHAIN-Prod.xml'
 SET @sqlStr = 'USE STORES_CHAIN; SELECT * from PRODUCTS FOR XML PATH(''PRODUCT''), ROOT(''PRODUCTS'') '
 SET @sqlCmd = 'bcp "' + @sqlStr + '" queryout ' + @fileName + ' -w -T'
 EXEC xp_cmdshell @sqlCmd;
+
+
+--import from xml to server 
+INSERT INTO PRODUCTS (Prod_Id, Prod_Name, Type_Prod_Id, Price_Purchase, Price_Sell, Weight, Volume)
+SELECT
+   MY_XML.PRODUCT.query('Prod_Id').value('.', 'NVARCHAR(13)'),
+   MY_XML.PRODUCT.query('Prod_Name').value('.', 'VARCHAR(50)'),
+   MY_XML.PRODUCT.query('Type_Prod_Id').value('.', 'INT'),
+   MY_XML.PRODUCT.query('Price_Purchase').value('.', 'numeric(6,2)'),
+   MY_XML.PRODUCT.query('Price_Sell').value('.', 'numeric(6,2)'),
+   MY_XML.PRODUCT.query('Weight').value('.', 'INT'),
+   MY_XML.PRODUCT.query('Volume').value('.', 'INT')
+
+FROM (SELECT CAST(MY_XML AS xml)
+      FROM OPENROWSET(BULK 'D:\Subject\BD_COURSE\backups\Import.xml', SINGLE_BLOB) AS T(MY_XML)) AS T(MY_XML)
+      CROSS APPLY MY_XML.nodes('PRODUCTS/PRODUCT') AS MY_XML (PRODUCT);
+
+
+select * from PRODUCTS where Prod_Id = '1111111111111' or Prod_Id = '1111111111112'
+delete from PRODUCTS where Prod_Id = '1111111111111' or Prod_Id = '1111111111112'
