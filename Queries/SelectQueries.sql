702605fb3bf5214bf1bd85d446c36c00589e67db
Develer12 <cenia-v@mail.ru>
2019-12-01 13:56:24 +0300

    Update selection

--- Queries/SelectQueries.sql
+++ Queries/SelectQueries.sql
@@ -4,42 +4,66 @@ use STORES_CHAIN
 --Watch all employees
 go
 Create Procedure SEmpl
+	@end int,
+	@start int
 AS
 Begin
-	SELECT Name_First, Name_Sec, Name_Father, t.Post, Sex, Age, Exp, Name_Store,County, Town, Adress FROM EMPLOYEES p
-	LEFT OUTER JOIN
-	POSTS  t ON t.Post_Id = p.Id_Post
-	LEFT OUTER JOIN
-	STORES  s ON s.Id_Store = p.Id_Store
-	LEFT OUTER JOIN
-	DEPARTMENTS d ON d.Dep_Id = p.Id_Dep;
+	WITH num_row AS
+	(
+		SELECT row_number() OVER (ORDER BY Name_First) as nom , 		
+		Name_First, Name_Sec, Name_Father, t.Post, Sex, Age, Exp, Name_Store,County, Town, Adress FROM EMPLOYEES p
+		LEFT OUTER JOIN
+		POSTS  t ON t.Post_Id = p.Id_Post
+		LEFT OUTER JOIN
+		STORES  s ON s.Id_Store = p.Id_Store
+		LEFT OUTER JOIN
+		DEPARTMENTS d ON d.Dep_Id = p.Id_Dep
+	) 
+	SELECT * FROM num_row
+	WHERE nom BETWEEN @start AND @end;
 End;
 
 
 --Watch all Products
 go
 Create Procedure SProd
+	@end int,
+	@start int
 AS
 Begin
-	SELECT Prod_Id, Prod_Name, Prod_Type,Depart,Price_Purchase,Price_Sell,Weight,Volume FROM PRODUCTS p
-	LEFT OUTER JOIN
-	PRODUCT_TYPE  t ON t.Prod_Type_Id = p.Type_Prod_Id
-	LEFT OUTER JOIN
-	DEPARTMENTS d ON d.Dep_Id = t.Id_Dep;
+	WITH num_row AS
+	(
+		SELECT row_number() OVER (ORDER BY Prod_Name) as nom , 		
+		Prod_Id, Prod_Name, Prod_Type,Depart,Price_Purchase,Price_Sell,Weight,Volume FROM PRODUCTS p
+		LEFT OUTER JOIN
+		PRODUCT_TYPE  t ON t.Prod_Type_Id = p.Type_Prod_Id
+		LEFT OUTER JOIN
+		DEPARTMENTS d ON d.Dep_Id = t.Id_Dep
+	) 
+	SELECT * FROM num_row
+	WHERE nom BETWEEN @start AND @end;
 End;
 
 
 --Watch all Stores
 go
 Create Procedure SStore
+	@end int,
+	@start int
 AS
 Begin
-	Select Name_Store, County, Town, Adress, Size, s.Type, count(Id_Empl) as Workers FROM STORES p
-	LEFT OUTER JOIN
-	TYPE_STORE  s ON s.Store_Type_Id = p.Id_Type
-	LEFT OUTER JOIN
-	EMPLOYEES  t ON t.Id_Store = p.Id_Type
-	group by Name_Store, County, Town, Adress, Size, s.Type;
+	WITH num_row AS
+	(
+		SELECT row_number() OVER (ORDER BY County) as nom , 		
+		Name_Store, County, Town, Adress, Size, s.Type, count(Id_Empl) as Workers FROM STORES p
+		LEFT OUTER JOIN
+		TYPE_STORE  s ON s.Store_Type_Id = p.Id_Type
+		LEFT OUTER JOIN
+		EMPLOYEES  t ON t.Id_Store = p.Id_Type
+		group by Name_Store, County, Town, Adress, Size, s.Type
+	) 
+	SELECT * FROM num_row
+	WHERE nom BETWEEN @start AND @end;
 End;
 
 
