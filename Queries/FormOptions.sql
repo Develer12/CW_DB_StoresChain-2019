322947617aadee48e7c1ef9c8a7964a4dd7bdd94
Develer12 <cenia-v@mail.ru>
2019-12-10 19:48:36 +0300

    Create options for form

---
+++ Queries/FormOptions.sql
@@ -0,0 +1,50 @@
+use STORES_CHAIN;
+
+
+go
+Create Procedure OptProdType
+	@dep nvarchar(50)
+AS
+Begin
+	SELECT Prod_Type FROM PRODUCT_TYPE where Id_Dep = (select Dep_Id from DEPARTMENTS where Depart = @dep)
+End;
+
+
+go
+Create Procedure OptDepart
+AS
+Begin
+	SELECT Depart FROM DEPARTMENTS
+End;
+
+
+go
+Create Procedure OptPost
+AS
+Begin
+	SELECT Post FROM POSTS
+End;
+
+
+go
+Create Procedure OptStoreType
+AS
+Begin
+	SELECT * FROM TYPE_STORE
+End;
+
+
+go
+Create Procedure OptUserType
+AS
+Begin
+	SELECT Type_User FROM USER_TYPE
+End;
+
+
+go
+Create Procedure OptAdminType
+AS
+Begin
+	SELECT Admin_Type FROM TYPE_ADMIN
+End;
